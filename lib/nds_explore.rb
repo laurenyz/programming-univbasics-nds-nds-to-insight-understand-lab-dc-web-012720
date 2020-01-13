fae77264b63ab173fdb7bac2f2a67d1a46522180
Lauren Yu <laurenkyu@gmail.com>
2020-01-13 15:22:04 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,20 +7,20 @@ def pretty_print_nds(nds)
   pp (nds)
 end
 
-def print_first_directors_movie_titles
+# def print_first_directors_movie_titles
   
-  movie_index = 0 
-  new_array = []
-  while movie_index < directors_database[0][:movies].length do
-  new_array << directors_database[0][:movies][movie_index][:title]
+#   movie_index = 0 
+#   new_array = []
+#   while movie_index < directors_database[0][:movies].length do
+#   new_array << directors_database[0][:movies][movie_index][:title]
   
-  movie_index += 1
+#   movie_index += 1
   
-  end   
+#   end   
   
-  print new_array.join("\n")
+#   print new_array.join("\n")
   
-end
+# end
 
 #pretty_print_nds(directors_database)
 print_first_directors_movie_titles
