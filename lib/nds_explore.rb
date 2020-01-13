4ac614fac879b13b0d12278f8a09495ece5fa272
Lauren Yu <laurenkyu@gmail.com>
2020-01-13 14:56:29 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,7 +8,15 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles(nds)
+  
+  movie_index = 0 
+  
+  while movie_index < nds[0][:movies].length do
   p nds[0][:movies][0][:title]
+  
+  movie_index += 1
+  
+  end   
 end
 
 #pretty_print_nds(directors_database)
